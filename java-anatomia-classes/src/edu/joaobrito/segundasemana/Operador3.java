022376bb4f4938622733b84b4c8e1b5177f0918f
tocattabrito <tocattabrito@gmail.com>
2024-07-29 18:08:49 -0300

    Backup de arquivos

---
+++ java-anatomia-classes/src/edu/joaobrito/segundasemana/Operador3.java
@@ -0,0 +1,20 @@
+package edu.joaobrito.segundasemana;
+
+public class Operador3 {
+
+    public static void main(String[] args) {
+        // Entrada
+        int numero = 5;
+        boolean variavel = true;
+
+
+        // Processamento
+        numero++; // numero = numero + 1;
+        variavel = !variavel; // inverte o valor lógico
+
+        // Saída
+        System.out.println(numero);
+        System.out.println(variavel); 
+        
+    }
+}
---
+++ java-anatomia-classes/src/edu/joaobrito/segundasemana/Operadores2.java
@@ -0,0 +1,14 @@
+package edu.joaobrito.segundasemana;
+
+public class Operadores2 {
+
+    public static void main(String[] args) {
+        
+        int numero = +5;
+
+        numero = numero * -1; // altera o sinal do resultado
+
+        System.out.println(numero);
+       
+    }
+}
