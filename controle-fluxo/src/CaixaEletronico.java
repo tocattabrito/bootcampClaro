b11d012479da92ffcf9510980d93ddfd918a40a6
tocattabrito <tocattabrito@gmail.com>
2024-08-03 19:37:44 -0300

    Operadores condicionais

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,21 @@
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if (valorSolicitado < saldo) {
+            
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + saldo);
+            
+        } else {
+
+            System.out.println("Saldo insuficiente!");
+        }
+            
+        System.out.println("Saldo atual: R$ "+saldo);
+    }
+    
+}
---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,21 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        
+        int nota = 8;
+
+        if (nota >= 7) { // true
+
+            System.out.println("Aprovado");
+
+        } else if (nota >=5 && nota < 7) { // true ou false
+
+            System.out.println("Recuperação");
+
+        } else {
+
+             System.out.println("Reprovado");
+
+        }
+    }
+}
