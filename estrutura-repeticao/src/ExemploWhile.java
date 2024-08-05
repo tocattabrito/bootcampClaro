e85e8993f4322919f9df237474ed9355d7e90617
tocattabrito <tocattabrito@gmail.com>
2024-08-04 23:33:49 -0300

    Exemplo de While

---
+++ estrutura-repeticao/src/ExemploWhile.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+
+    public static void main(String[] args) {
+
+        double mesada = 50.0;
+        
+        while (mesada > 0) {
+
+            double valorDoce = valorAleatorio();
+
+            if (valorDoce > mesada)
+                valorDoce = mesada;
+
+            mesada -= valorDoce;
+            System.out.println("Valor do doce: " + valorDoce + " Adicionado ao carrinho");
+        }
+
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+
+    }
+
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+    
+}
