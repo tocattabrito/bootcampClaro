da42432800bfb9fe4f06ff467575d48757ce6e2c
tocattabrito <tocattabrito@gmail.com>
2024-08-06 22:00:57 -0300

    Estudo dos conceitos da Programação Orientada a Objeto

---
+++ FundamentosPOOJava/src/msnmessenger/MSNMensseger.java
@@ -0,0 +1,17 @@
+package msnmessenger;
+
+public class MSNMensseger {
+
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectando a internet");
+    }
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
