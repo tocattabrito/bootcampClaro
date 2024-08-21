3ce78a1c045eeb89e6e53e242dd45b944a85d37b
tocattabrito <tocattabrito@gmail.com>
2024-08-21 04:36:25 -0300

    Update Primeiros passos Spring Boot

--- SpringBoot/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ SpringBoot/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -6,13 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
-
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = repository.findByNameContaining("João Brito");
+        for(User u: users) {
+            System.out.println(u);
+        }
+    }
+    private void insertUser(){
         User user = new User();
         user.setName("Carlos Eduardo");
         user.setUsername("cadu");
